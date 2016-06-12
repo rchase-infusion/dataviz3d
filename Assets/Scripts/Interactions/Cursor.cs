e7b0b8963792f448b4334d3a3d92bd466437e92b
Youenn-Bouglouan <youenn.bouglouan@gmail.com>
2016-06-12 16:28:32 +0200

    added gitignore, Assets and project files

---
+++ Assets/Scripts/Interactions/Cursor.cs
@@ -0,0 +1,41 @@
+﻿using Assets.Scripts.Utils;
+using UnityEngine;
+
+namespace Assets.Scripts.Interactions
+{
+    [RequireComponent(typeof(MeshRenderer))]
+    public class Cursor : MonoBehaviour
+    {
+        private Camera _camera;
+        private MeshRenderer _meshRenderer;
+
+        public float MaxHitDetectionDistance = 5;
+        public Color DefaultColor = Color.white;
+        public Color HoverColor = Color.red;
+        
+        void Start()
+        {
+            _camera = Camera.main;
+            _meshRenderer = gameObject.GetComponent<MeshRenderer>();
+            _meshRenderer.material.color = DefaultColor;
+        }
+
+        void Update()
+        {
+            var cameraPosition = _camera.transform.position;
+            var forwardDirection = _camera.transform.forward;
+
+            RaycastHit hit;
+            if (Physics.Raycast(cameraPosition, forwardDirection, out hit, MaxHitDetectionDistance))
+            {
+                gameObject.SetPosition(hit.point);
+                gameObject.transform.rotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
+                _meshRenderer.enabled = true;
+            }
+            else
+            {
+                _meshRenderer.enabled = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SceneUtilities/StartMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.SceneUtilities
+{
+    public class StartMenu : MonoBehaviour
+    {
+        public void LoadGraphScene()
+        {
+            Load(1);
+        }
+
+        private void Load(int levelId)
+        {
+            SceneManager.LoadScene(levelId);
+        }
+    }
+}
