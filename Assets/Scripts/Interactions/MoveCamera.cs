cc48f90eef68783b6c17ca104fddb751d31bb2fe
Youenn-Bouglouan <youenn.bouglouan@gmail.com>
2016-06-12 18:34:38 +0200

    added basic Camera Look (up/down/left/right) functionality for Unity

--- Assets/Scripts/Interactions/MoveCamera.cs
+++ Assets/Scripts/Interactions/MoveCamera.cs
@@ -1,15 +1,39 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System;
+using UnityEngine;
 
-public class MoveCamera : MonoBehaviour {
+namespace Assets.Scripts.Interactions
+{
+    public class MoveCamera : MonoBehaviour
+    {
+        [Tooltip("Speed at which the camera looks left/right")]
+        public float HorizontalLookSpeed = 50;
+        [Tooltip("Speed at which the camera looks up/down")]
+        public float VerticalLookSpeed = 50;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        private void Start()
+        { }
+
+        void Update()
+        {
+            if (Input.GetMouseButton(1))
+            {
+                LookLeftRight();
+                LookUpDown();
+            }
+        }
+
+        private void LookLeftRight()
+        {
+            var horizontalMouseInput = Input.GetAxis("Mouse X");
+            if (Math.Abs(horizontalMouseInput) > 0.1)
+                transform.Rotate(Vector3.up * Mathf.Sign(horizontalMouseInput) * HorizontalLookSpeed * Time.deltaTime);
+        }
+
+        private void LookUpDown()
+        {
+            var verticalMouseInput = Input.GetAxis("Mouse Y");
+            if (Math.Abs(verticalMouseInput) > 0.1)
+                transform.Rotate(Vector3.right * -Mathf.Sign(verticalMouseInput) * HorizontalLookSpeed * Time.deltaTime);
+        }
+    }
 }
