d7d407ff009823893e08d8522a22d98b57a8cd70
Youenn-Bouglouan <youenn.bouglouan@gmail.com>
2016-06-15 15:18:44 +0200

    added first voice commands: reset and text + sound notification

---
+++ Assets/Scripts/Interactions/VoiceCommandsManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Assets.Scripts.SceneUtilities;
+using HoloToolkit.Unity;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+namespace Assets.Scripts.Interactions
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class VoiceCommandsManager : MonoBehaviour
+    {
+        private KeywordRecognizer _keywordRecognizer;
+        private Dictionary<string, System.Action> _commands;
+        private AudioSource _audioSource;
+        
+        public ResetScene ResetScene;
+        public InitializeGraphV2 Graph;
+        public ToggleLabels ToggleLabels;
+
+        private void Start()
+        {
+            if (ResetScene == null)
+                throw new NullReferenceException("[VoiceCommandsManager.Start] Reference to ResetScene not set!");
+            if (Graph == null)
+                throw new NullReferenceException("[VoiceCommandsManager.Start] Reference to InitializeGraphV2 not set!");
+            if (ToggleLabels == null)
+                throw new NullReferenceException("[VoiceCommandsManager.Start] Reference to ToggleLabels not set!");
+
+            _commands = new Dictionary<string, Action>();
+            _audioSource = gameObject.AddComponent<AudioSource>();
+            _audioSource.clip = Resources.Load<AudioClip>("command_sound");
+            
+            // Reset scene
+            _commands.Add("Reset", () =>
+            {
+                ResetScene.Reset();
+            });
+
+            // Toggle labels
+            _commands.Add("Text", () =>
+            {
+                _audioSource.Play();
+                ToggleLabels.Toggle();
+            });
+
+            // Initialize the keyword recognizer
+            _keywordRecognizer = new KeywordRecognizer(_commands.Keys.ToArray());
+            _keywordRecognizer.OnPhraseRecognized += CallCommand;
+            _keywordRecognizer.Start();
+        }
+
+        private void CallCommand(PhraseRecognizedEventArgs args)
+        {
+            Action command;
+            if (_commands.TryGetValue(args.text, out command))
+            {
+                command.Invoke();
+            }
+        }
+    }
+}
